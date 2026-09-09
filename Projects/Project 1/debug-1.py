9cef2e95929d751e11bf8054e10d349768bfadf8
DerekJ0y <derrk.joy04@gmail.com>
2026-09-09 14:28:52 -0600

    built program 1 and fixed debug 1

--- Projects/Project 1/debug-1.py
+++ Projects/Project 1/debug-1.py
@@ -4,10 +4,8 @@
 # This program should add three numbers together.
 
 total = 0
-num1 = input("What's the first number? >")
-total = num1
-num2 = input("What's the second number? >")
-total = num2
-num3 = input("What's the third number? >")
-total = num3
+num1 = float(input("What's the first number? >"))
+num2 = float(input("What's the second number? >")) # num1,2,and 3 needed to be converted to strings to be added together
+num3 = float(input("What's the third number? >"))
+total = num1+num2+num3 # instaed of just changing the total variable the 3 num variables needed to get added together
 print(f"Total is: {total}")
