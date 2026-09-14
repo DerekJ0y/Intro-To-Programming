d4c20c38f04831f3f83303744a7c3b2d5bb977cd
DerekJ0y <derrk.joy04@gmail.com>
2026-09-13 22:31:47 -0600

    changed name of program 1 and 2 and finished program 1

--- Projects/Project 1/program 1.py
+++ Projects/Project 1/program 1.py
@@ -1,10 +1,4 @@
-# budget calculator
-income=float(input('what is your average monthly income: ')) #asking for user imput for there monthly income, there rent, there grocery bill, other daily exspences, and what percent they want to put in saving and them giving them a total on there remaining balence 
-save_percent= float(input('what persent of your income do you want to save: '))
-amount_saved=income/save_percent
-rent=float(input('how much is your monthly rent: '))
-grocery_bill=float(input('what is your average grocery bill: '))
-other_expences=float(input('what is your average other expences like phone bill: '))
-
-sum=((((income-amount_saved)-rent)-grocery_bill)-other_expences)
-print(sum)
+# grocery list
+Grocery_list= ['Flat Bread','Lettuce']
+Grocery_list.append(input(f'The list right now is {Grocery_list}, please add an item that you want me to grab for you:  '))
+print(f'The list is {Grocery_list}')
